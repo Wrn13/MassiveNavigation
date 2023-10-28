385aa324ae10cec92faf2c3233b1ef7d1e03c41a
PDempsey03 <123753503+PDempsey03@users.noreply.github.com>
2023-10-28 19:59:57 -0400

    updated images

--- app/src/main/java/nisaefendioglu/snapchat/Fragment/Chat.java
+++ app/src/main/java/nisaefendioglu/snapchat/Fragment/Chat.java
@@ -29,20 +29,20 @@ public class Chat extends Fragment {
         view = inflater.inflate(R.layout.chatlist, container, false);
 
         chatList = view.findViewById(R.id.chatList);
-        chatListArrayList.add(new ChatList("West Plaza","Tap to navigate",R.drawable.avatar8));
-        chatListArrayList.add(new ChatList("Union Market","Tap to navigate",R.drawable.avatar9));
-        chatListArrayList.add(new ChatList("Espress-oh","Tap to navigate",R.drawable.avatar));
-        chatListArrayList.add(new ChatList("Woody's Tavern","Tap to navigate",R.drawable.avatar3));
-        chatListArrayList.add(new ChatList("Brutus Buckeye Statue","Tap to navigate",R.drawable.avatar6));
-        chatListArrayList.add(new ChatList("Ben and Arlene Lounge","Tap to navigate",R.drawable.avatar4));
-        chatListArrayList.add(new ChatList("Sloopy's Diner","Tap to navigate",R.drawable.avatar5));
-        chatListArrayList.add(new ChatList("US Bank Conference Theater","Tap to navigate",R.drawable.avatar2));
-        chatListArrayList.add(new ChatList("Performance Hall","Tap to navigate",R.drawable.avatar));
-        chatListArrayList.add(new ChatList("Information Center","Tap to navigate",R.drawable.avatar8));
-        chatListArrayList.add(new ChatList("Multi-Cultural Center","Tap to navigate",R.drawable.avatar9));
-        chatListArrayList.add(new ChatList("Book Store","Tap to navigate",R.drawable.avatar));
-        chatListArrayList.add(new ChatList("US Bank","Tap to navigate",R.drawable.avatar3));
-        chatListArrayList.add(new ChatList("Great Hall Meeting Room","Tap to navigate",R.drawable.avatar6));
+        chatListArrayList.add(new ChatList("West Plaza","Tap to navigate",R.drawable.westplaza));
+        chatListArrayList.add(new ChatList("Union Market","Tap to navigate",R.drawable.unionmarket));
+        chatListArrayList.add(new ChatList("Espress-oh","Tap to navigate",R.drawable.espressooh));
+        chatListArrayList.add(new ChatList("Woody's Tavern","Tap to navigate",R.drawable.woodystavern));
+        chatListArrayList.add(new ChatList("Brutus Buckeye Statue","Tap to navigate",R.drawable.brutusstatue));
+        chatListArrayList.add(new ChatList("Ben and Arlene Lounge","Tap to navigate",R.drawable.benandarleenlounge));
+        chatListArrayList.add(new ChatList("Sloopy's Diner","Tap to navigate",R.drawable.sloopysdiner));
+        chatListArrayList.add(new ChatList("US Bank Conference Theater","Tap to navigate",R.drawable.usbankconferencetheater));
+        chatListArrayList.add(new ChatList("Performance Hall","Tap to navigate",R.drawable.performancehall));
+        chatListArrayList.add(new ChatList("Information Center","Tap to navigate",R.drawable.informationdesk));
+        chatListArrayList.add(new ChatList("Multi-Cultural Center","Tap to navigate",R.drawable.multiculturalcenter));
+        chatListArrayList.add(new ChatList("Book Store","Tap to navigate",R.drawable.bookstore));
+        chatListArrayList.add(new ChatList("US Bank","Tap to navigate",R.drawable.usbank));
+        chatListArrayList.add(new ChatList("Great Hall Meeting Room","Tap to navigate",R.drawable.greathall));
 
         ChatAdapter chatAdapter = new ChatAdapter(getActivity(), chatListArrayList);
         if (chatList != null) {
