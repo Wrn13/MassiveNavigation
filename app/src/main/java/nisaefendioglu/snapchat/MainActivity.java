e6584ad6486f1f4b59db2fa02fc01e6c84d980ba
amazingabhinayyy <51940031+amazingabhinayyy@users.noreply.github.com>
2023-10-28 23:48:24 -0400

    GPS Positions
    Button for GPS positions

--- app/src/main/java/nisaefendioglu/snapchat/MainActivity.java
+++ app/src/main/java/nisaefendioglu/snapchat/MainActivity.java
@@ -1,31 +1,60 @@
 package nisaefendioglu.snapchat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
-import android.util.Size;
+import android.os.Looper;
 import android.view.View;
 import android.widget.ImageView;
-
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.viewpager.widget.ViewPager;
-
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
 import nisaefendioglu.snapchat.Adapter.MainPagerAdapter;
 import nisaefendioglu.snapchat.Fragment.Camera;
-import nisaefendioglu.snapchat.Fragment.Chat;
-import nisaefendioglu.snapchat.Fragment.Stories;
+import androidx.annotation.NonNull;
+import android.content.IntentSender;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    ImageView CaptureBtn,chat_btn,story_btn,settings;
+    ImageView CaptureBtn, chat_btn, story_btn, settings;
 
+    private TextView AddressText;
+    private Button LocationButton;
+    private LocationRequest locationRequest;
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final ViewPager viewPager = findViewById(R.id.ma_view_pager);
+        AddressText = findViewById(R.id.addressText);
+        LocationButton = findViewById(R.id.locationButton);
+
+        locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(5000);
+        locationRequest.setFastestInterval(2000);
+
+        /*final ViewPager viewPager = findViewById(R.id.ma_view_pager);
 
         MainPagerAdapter mainPagerAdapter = new MainPagerAdapter(getSupportFragmentManager());
         viewPager.setAdapter(mainPagerAdapter);
@@ -64,9 +93,100 @@ public class MainActivity extends AppCompatActivity {
                     viewPager.setCurrentItem(3, true);
                 }
             }
+        });*/
+
+
+
+        LocationButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                        if (isGPSEnabled()) {
+                            LocationServices.getFusedLocationProviderClient(MainActivity.this)
+                                    .requestLocationUpdates(locationRequest, new LocationCallback() {
+                                        @Override
+                                        public void onLocationResult(@NonNull LocationResult locationResult) {
+                                            super.onLocationResult(locationResult);
+
+                                            LocationServices.getFusedLocationProviderClient(MainActivity.this)
+                                                    .removeLocationUpdates(this);
+
+                                            if (locationResult != null && locationResult.getLocations().size() > 0) {
+                                                int index = locationResult.getLocations().size() - 1;
+                                                double latitude = locationResult.getLocations().get(index).getLatitude();
+                                                double longitude = locationResult.getLocations().get(index).getLongitude();
+                                                System.out.println("Latitude: " + latitude + "/n" + "Longitude: " + longitude);
+                                                AddressText.setText("Latitude: " + latitude + "n" + "Longitude: " + longitude);
+                                            }
+                                        }
+                                    }, Looper.getMainLooper());
+                        } else {
+                            turnOnGPS();
+                        }
+                    } else {
+                        requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+                    }
+                }
+
+            }
         });
+    }
+
+            private boolean isGPSEnabled() {
+                LocationManager locationManager = null;
+                boolean isEnabled = false;
+
+                if (locationManager == null) {
+                    locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+                }
+
+                isEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+                return isEnabled;
+
+        }
 
 
+    private void turnOnGPS() {
+
+
+
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(locationRequest);
+        builder.setAlwaysShow(true);
+
+        Task<LocationSettingsResponse> result = LocationServices.getSettingsClient(getApplicationContext())
+                .checkLocationSettings(builder.build());
+
+        result.addOnCompleteListener(new OnCompleteListener<LocationSettingsResponse>() {
+            @Override
+            public void onComplete(@NonNull Task<LocationSettingsResponse> task) {
+
+                try {
+                    LocationSettingsResponse response = task.getResult(ApiException.class);
+                    Toast.makeText(MainActivity.this, "GPS is already tured on", Toast.LENGTH_SHORT).show();
+
+                } catch (ApiException e) {
+
+                    switch (e.getStatusCode()) {
+                        case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+
+                            try {
+                                ResolvableApiException resolvableApiException = (ResolvableApiException) e;
+                                resolvableApiException.startResolutionForResult(MainActivity.this, 2);
+                            } catch (IntentSender.SendIntentException ex) {
+                                ex.printStackTrace();
+                            }
+                            break;
+
+                        case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                            //Device does not have location
+                            break;
+                    }
+                }
+            }
+        });
 
     }
 
